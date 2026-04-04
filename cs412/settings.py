dc56cbc33fc663803cbf1ea8dbaaeaf882bfdd2a
thearhansheth <arhan.sheth@gmail.com>
2026-04-03 20:22:45 -0400

    Assignment 9 completed

--- cs412/settings.py
+++ cs412/settings.py
@@ -141,3 +141,8 @@ if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+  'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+  'PAGE_SIZE': 10
+}
